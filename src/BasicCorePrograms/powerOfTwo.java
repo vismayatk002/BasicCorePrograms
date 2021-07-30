8223b87cf8055dab0abaa395b34737ddda22c1cc
vismayatk002 <vismayatk002@gmail.com>
2021-07-30 17:00:40 +0530

    Updated with using commandline argument

--- src/BasicCorePrograms/powerOfTwo.java
+++ src/BasicCorePrograms/powerOfTwo.java
@@ -1,5 +1,7 @@
 package BasicCorePrograms;
 
+//import java.util.Scanner;
+
 public class powerOfTwo {
 	public static void main(String[] args) {
 		
@@ -7,6 +9,10 @@ public class powerOfTwo {
 		//read input using command line argument
 		//convert command line argument to integer
 		int N =Integer.parseInt(args[0]);
+		//for testing
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("Enter the limit ");
+//		int N = sc.nextInt();
         if( N >= 0  && N <= 31){
 
            for(int i= 0; i<= N; i++){
