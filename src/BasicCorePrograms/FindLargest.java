e4d0615fed5d9877cdb978eac42e0c5567046362
vismayatk002 <vismayatk002@gmail.com>
2021-08-02 18:59:05 +0530

    Add : method to find largest number

--- src/BasicCorePrograms/FindLargest.java
+++ src/BasicCorePrograms/FindLargest.java
@@ -4,15 +4,7 @@ import java.util.Scanner;
 
 public class FindLargest {
 	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter First number : ");
-		int number1 = sc.nextInt();
-		System.out.print("Enter Second number: ");
-		int number2 = sc.nextInt();
-		System.out.print("Enter Third number : ");
-		int number3 = sc.nextInt();
+	public static void findLargest(int number1 , int number2 , int number3) {
 		if(number1 > number2 && number1 > number3) {
 			System.out.println("Largest Number : " + number1);
 		}
@@ -22,6 +14,17 @@ public class FindLargest {
 		else {
 			System.out.println("Largest Number : " + number3);
 		}
+	}
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter First number : ");
+		int number1 = sc.nextInt();
+		System.out.print("Enter Second number: ");
+		int number2 = sc.nextInt();
+		System.out.print("Enter Third number : ");
+		int number3 = sc.nextInt();
+		findLargest(number1 ,number2,number3);
 		sc.close();
 	}
 }
