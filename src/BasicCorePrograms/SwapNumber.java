f9211981da1ed7f735a2f53970007b91d245f4d4
vismayatk002 <vismayatk002@gmail.com>
2021-08-02 19:28:04 +0530

    Add : method to swap two numbers

--- src/BasicCorePrograms/SwapNumber.java
+++ src/BasicCorePrograms/SwapNumber.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class SwapNumber {
 	
+	public static void swap(int number1, int number2) {
+		
+		//without using temporary variable
+		number1 = number1 + number2;
+		number2 = number1 - number2;
+		number1 = number1 - number2;
+		System.out.println("After Swapping numbers :" +number1 + "," + number2);
+	}
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -12,11 +20,7 @@ public class SwapNumber {
 		System.out.print("Enter Second number: ");
 		int number2 = sc.nextInt();
 		System.out.println("Numbers :" + number1 + "," + number2);
-		//without using temporary variable
-		number1 = number1 + number2;
-		number2 = number1 - number2;
-		number1 = number1 - number2;
-		System.out.println("After Swapping numbers :" +number1 + "," + number2);
+		swap(number1 ,number2);
 		sc.close();
 	}
 }
