31478a3888323eb669e13e8b59b3738e825b9e8b
vismayatk002 <vismayatk002@gmail.com>
2021-08-02 18:45:30 +0530

    Add : method to check vowel or consonant

--- src/BasicCorePrograms/Alphabet.java
+++ src/BasicCorePrograms/Alphabet.java
@@ -2,7 +2,16 @@ package BasicCorePrograms;
 
 import java.util.Scanner;
 public class Alphabet {
-
+	
+	public static void isVowelOrConsonant(char alpha) {
+		
+		if(alpha == 'a' || alpha == 'e'|| alpha == 'i' || alpha == 'o' || alpha == 'u' ) {
+			System.out.println("Vowel");
+		}
+		else {
+			System.out.println("Consonant");
+		}
+	}
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -10,12 +19,7 @@ public class Alphabet {
 		System.out.print("Enter a character :");
 		//store the character input in to variable 
 	    char alpha = sc.next().charAt(0);
-		if(alpha == 'a' || alpha == 'e'|| alpha == 'i' || alpha == 'o' || alpha == 'u' ) {
-			System.out.println("Vowel");
-		}
-		else {
-			System.out.println("Consonant");
-		}
+	    isVowelOrConsonant(alpha);
 		sc.close();
 	}
 }
